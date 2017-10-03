8844c0328943142cfcf0dc7af4d753afc613302e
MIREYA <MIREYA@HP>
2017-10-02 20:20:45 -0500

    Add project files.

---
+++ App8/App8/Page2.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App8
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page2 : ContentPage
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+        private void Button_Clicked1(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new Page3());
+        }
+
+        private void datos(object sender, EventArgs e)
+        {
+            var nombre = txtnombre.Text;
+            var apellido = txtapellido.Text;
+            var direccion = txtdireccion.Text;
+            var telefono = txttelefono.Text;
+            var carrera = txtcarrera.Text;
+            var semestre = txtsemestre.Text;
+            var matricula = txtmatricula.Text;
+            var correo = txtcorreo.Text;
+            var github = txtgithub.Text;
+
+
+            DisplayAlert("Datos", "Datos Personales\n Nombre: " + nombre + "\n Apellidos: " + apellido + "\n Direccion:" + direccion
+            + "\n Telefono:" + telefono + "\n Datos Escolares\n Carrera:" + carrera + "\n Semestre:" + semestre + "\n Matricula:" + matricula +
+            "\n Datos Sociales\n Correo:" + correo + "\n Github:" + github, "OK");
+        }
+    }
+}
---
+++ App8/App8/Page3.xaml.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App8
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page3 : ContentPage
+    {
+        double var1 = 0, var2 = 0;
+        string opcion;
+        public Page3()
+        {
+            InitializeComponent();
+        }
+        public void resultados()
+        {
+            var2 = Convert.ToDouble(resul.Text);
+            if (opcion == "suma")
+            {
+                resul.Text = var1 + var2 + "";
+            }
+            else
+            if (opcion == "resta")
+            {
+                resul.Text = var1 - var2 + "";
+            }
+            else
+            if (opcion == "multi")
+            {
+                resul.Text = var1 * var2 + "";
+            }
+            else
+            if (opcion == "div")
+            {
+                resul.Text = var1 / var2 + "";
+            }
+            else
+            if (opcion == "porciento")
+            {
+                resul.Text = var1 % var2 + "";
+            }
+
+        }
+
+        private void Borrar_Clicked(object sender, EventArgs e)
+        {
+            resul.Text = "";
+        }
+
+        private void masmenos_Clicked(object sender, EventArgs e)
+        {
+            double comprobacion = Convert.ToDouble(resul.Text);
+            if (comprobacion >= 0)
+            {
+                resul.Text = "-" + comprobacion;
+            }
+            else if (comprobacion <= 0)
+            {
+                resul.Text = -1 * comprobacion + "";
+            }
+
+        }
+
+        private void porciento_Clicked(object sender, EventArgs e)
+        {
+            var1 = Convert.ToDouble(resul.Text);
+            opcion = "porciento";
+            resul.Text = "";
+        }
+
+        private void division_Clicked(object sender, EventArgs e)
+        {
+            var1 = Convert.ToDouble(resul.Text);
+            opcion = "div";
+            resul.Text = "";
+        }
+
+        private void siete_Clicked(object sender, EventArgs e)
+        {
+            resul.Text = resul.Text + 7;
+
+        }
+
+        private void ocho_Clicked(object sender, EventArgs e)
+        {
+            resul.Text = resul.Text + 8;
+        }
+
+        private void nueve_Clicked(object sender, EventArgs e)
+        {
+            resul.Text = resul.Text + 9;
+        }
+
+        private void multi_Clicked(object sender, EventArgs e)
+        {
+            var1 = Convert.ToDouble(resul.Text);
+            opcion = "multi";
+            resul.Text = "";
+
+        }
+
+        private void cuatro_Clicked(object sender, EventArgs e)
+        {
+            resul.Text = resul.Text + 4;
+        }
+
+        private void cinco_Clicked(object sender, EventArgs e)
+        {
+            resul.Text = resul.Text + 5;
+        }
+
+        private void seis_Clicked(object sender, EventArgs e)
+        {
+            resul.Text = resul.Text + 6;
+        }
+
+        private void menos_Clicked(object sender, EventArgs e)
+        {
+            var1 = Convert.ToDouble(resul.Text);
+            opcion = "resta";
+            resul.Text = "";
+        }
+
+        private void uno_Clicked(object sender, EventArgs e)
+        {
+            resul.Text = resul.Text + 1;
+        }
+
+        private void dos_Clicked(object sender, EventArgs e)
+        {
+            resul.Text = resul.Text + 2;
+        }
+
+        private void tres_Clicked(object sender, EventArgs e)
+        {
+            resul.Text = resul.Text + 3;
+        }
+
+        private void mas_Clicked(object sender, EventArgs e)
+        {
+            var1 = Convert.ToDouble(resul.Text);
+            opcion = "suma";
+            resul.Text = "";
+
+        }
+
+        private void cero_Clicked(object sender, EventArgs e)
+        {
+            resul.Text = resul.Text + 0;
+        }
+
+        private void punto_Clicked(object sender, EventArgs e)
+        {
+            if (resul.Text.Contains("."))
+            {
+                resul.Text = resul.Text;
+
+            }
+            else
+            {
+                resul.Text = resul.Text + ".";
+            }
+        }
+
+        async void Regresar_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new MainPage());
+        }
+
+        private void igual_Clicked(object sender, EventArgs e)
+        {
+
+            resultados();
+        }
+    }
+}
